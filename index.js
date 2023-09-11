9c9cfb4d3a6d4f9baa92f5d217c086e7ab5411a5
BomsG <120387032+BomsG@users.noreply.github.com>
2023-09-11 21:38:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -39,10 +39,10 @@ var images = [];
 var time = 2500; // the time it takes to change//
 
 //image list//
-images[0] = `/img-1-.png`;
-images[1] = `/img-2-.png`;
-images[2] = `/img-3-.png`;
-images[3] = `/img.last.png`;
+images[0] = `img-1-.png`;
+images[1] = `img-2-.png`;
+images[2] = `img-3-.png`;
+images[3] = `img.last.png`;
 
 //function to change the images//
 function changeImg() {
